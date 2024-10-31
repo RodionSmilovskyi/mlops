6f29e1bedc59ae847f08479550fd94574fd8ea17
Rodion Smilovskyi <smelovsky86@gmail.com>
2024-10-31 18:59:22 -0400

    Now function saves each ticker in separate file

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # pylint: disable=C0114, E0401, W0621
-import flask
-import functions_framework
+from io import BytesIO
 import logging
 from datetime import datetime, timedelta
-from typing import Final
+from typing import Final, cast
+import flask
+import functions_framework
 import pandas as pd
 import yfinance as yf  # type: ignore
 from yfinance import shared
@@ -15,7 +16,7 @@ TICKERS: Final[list[str]] = ["AMAT", "QCOM"]
 def upload_to_bucket(
     storage_client: storage.Client,
     bucket_name: str,
-    source_file_name: str,
+    df_string: str,
     destination_blob_name: str,
     logger: logging.Logger,
 ):
@@ -23,29 +24,45 @@ def upload_to_bucket(
     try:
         bucket = storage_client.bucket(bucket_name)
         blob = bucket.blob(destination_blob_name)
-        blob.upload_from_filename(source_file_name)
+        blob.upload_from_string(df_string)
 
-        logger.info(f"File {source_file_name} uploaded to {destination_blob_name}.")
+        logger.info(f"Dataframe uploaded to {destination_blob_name}.")
     except Exception as e:
         logger.error(f"Upload failed {e}")
 
 
+def dowload_saved_data(
+    storage_client: storage.Client,
+    bucket_name: str,
+    ticker: str,
+    logger: logging.Logger,
+) -> pd.DataFrame | None:
+    """Download previously saved ticker data"""
+    try:
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(f"{ticker}.csv")
+        byte_data = blob.download_as_bytes()
+        return pd.read_csv(BytesIO(byte_data), index_col="Date", parse_dates=True)
+    except Exception as e:
+        logger.error(f"Error downloading saved data for ticker {ticker} {e}")
+        return None
+
+
 def download_financial_data(
-    tickers: list[str], days: int, logger: logging.Logger
+    tickers: str, days: int, logger: logging.Logger
 ) -> pd.DataFrame | None:
     """Function to dowload data from yahoo finance"""
 
-    ticker_str: str = " ".join(TICKERS)
-    logger.info(f"Started downloading data for tickers {ticker_str} for {days} days")
+    logger.info(f"Started downloading data for tickers {tickers} for {days} days")
 
     try:
         today = datetime.now()
         start_date = today - timedelta(days=days)
 
-        data: pd.DataFrame = yf.download(ticker_str, start=start_date)
+        data: pd.DataFrame = yf.download(tickers, start=start_date, group_by="ticker")
 
         if not shared._ERRORS:
-            logger.info(f"Data for tickers {ticker_str} successfully downloaded")
+            logger.info(f"Data for tickers {tickers} successfully downloaded")
             return data
         else:
             logger.error(f"Data download failed {shared._ERRORS}")
@@ -71,21 +88,47 @@ def upload(request: flask.Request) -> flask.typing.ResponseReturnValue:
     days = request.args.get("days", 1, type=int)
     project = request.args.get("project", type=str)
     bucket = request.args.get("bucket", type=str)
-    
+    tickers = request.args.get("tickers", type=str, default="")
+
     if project is None:
         return ("project is not provided", 500)
-    
+
     if bucket is None:
         return ("bucket is not provided", 500)
-    
-    data = download_financial_data(TICKERS, days, logger)
+
+    if len(tickers) == 0:
+        return ("tickers are not provided", 500)
+
+    data = download_financial_data(tickers, days, logger)
 
     if data is not None:
-        data.to_csv("data.csv")
-        
         storage_client = storage.Client(project)
-        upload_to_bucket(
-            storage_client, bucket, "data.csv", "data.csv", logger
-        )
 
-    return ("Data updated successfully", 200)
+        for t in tickers.split(","):
+            ticker_name = t.strip().lower()
+            new_ticker_df = cast(pd.DataFrame, data[ticker_name.upper()])
+            saved_ticker_df = dowload_saved_data(
+                storage_client, bucket, ticker_name, logger
+            )
+
+            if saved_ticker_df is not None and saved_ticker_df.empty is False:
+                merged_df = saved_ticker_df.combine_first(new_ticker_df)
+                upload_to_bucket(
+                    storage_client,
+                    bucket,
+                    merged_df.to_csv(),
+                    f"{ticker_name}.csv",
+                    logger,
+                )
+            else:
+                upload_to_bucket(
+                    storage_client,
+                    bucket,
+                    new_ticker_df.to_csv(),
+                    f"{ticker_name}.csv",
+                    logger,
+                )
+
+        return ("Data updated successfully", 200)
+    else:
+        return ("Data update error", 500)
